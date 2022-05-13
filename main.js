e3a381639264275725a6c19ac8c48730a0ae042b
Ramfi <98537020+ramfiDev@users.noreply.github.com>
2022-05-13 19:02:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const socialMediaLinks = {
+  github: 'ramfiDev',
+  youtube: 'channel/UCIQ6AffoT_H04L4xMujoqQw',
+  twitter: 'Ramfiii'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${socialMediaLinks[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${socialMediaLinks.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      pageTitle.textContent = `NLW 2021 - Crachá de ${data.name}`
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
